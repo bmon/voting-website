aef97a9d3758f9c0c98a4908903b68e5cb577823
Brendan Roy <br3ndanr@gmail.com>
2020-02-25 19:53:53 +1100

    add css

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ PROJECT_ID=brendanr-misc
 SERVICE_NAME=emote-voting
 REGION=asia-northeast1
 # load env config from .env file and format it ready for use with gcloud. PORT is a reserved var and must be filtered.
-ENVCONF=$(cat .env | grep -Ev '^(PORT)' | tr '\n' ',')
+ENVCONF=$(cat .env | grep -v PORT | grep -v GOOGLE_APPLICATION_CREDENTIALS | tr '\n' ',')
 
 gcloud builds submit --tag gcr.io/$PROJECT_ID/$SERVICE_NAME
 gcloud run deploy $SERVICE_NAME --image gcr.io/$PROJECT_ID/$SERVICE_NAME --platform managed --set-env-vars=$ENVCONF --region=$REGION
